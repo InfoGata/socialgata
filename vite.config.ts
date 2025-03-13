5741caf4580e21c4cb6c42dca1b7c9d11658d2e3
Elijah Green <elijah@infogata.com>
2025-03-12 21:06:11 -0700

    Use @tanstack/router-plugin in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite({ target: "react" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
