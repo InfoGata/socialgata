ecfd5d24ac4857ff22e62e22a13584bd4085ac98
Elijah Green <elijah@infogata.com>
2025-03-15 18:17:44 -0700

    Move cors server to main project

--- cors-server.js
+++ cors-server.js
@@ -1,11 +1,10 @@
+import { createServer } from "cors-anywhere";
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || "0.0.0.0";
+var host = "0.0.0.0";
 // Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8085;
+var port = 8085;
 
-var cors_proxy = require("cors-anywhere");
-cors_proxy
-    .createServer({
+createServer({
         originWhitelist: [], // Allow all origins
         requireHeader: ["origin", "x-requested-with"],
         removeHeaders: ["cookie", "cookie2"],
