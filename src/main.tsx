b215424f8533485ae563310f698c0a2979ae21e4
Elijah Green <elijah@infogata.com>
2026-02-27 20:29:01 -0800

    Change ThemeProvider defaultTheme to "system"

--- src/main.tsx
+++ src/main.tsx
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       }}
     >
       <Provider store={store}>
-        <ThemeProvider defaultTheme="light">
+        <ThemeProvider defaultTheme="system">
           <PluginsProvider>
             <FavoritesRepoProvider>
               <FavoritesProvider>
