d3f55b4d45e89b157309b400fbfe5e815f46b0a4
Elijah Green <elijah@infogata.com>
2026-03-06 12:28:19 -0800

    Update ESLint 8 to 9 with flat config migration

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**", "node_modules/**", "public/**", "cors-server.js"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs["recommended-latest"],
+  {
+    ...reactRefresh.configs.vite,
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  }
+);
